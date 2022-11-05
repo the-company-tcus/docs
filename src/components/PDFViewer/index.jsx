4c5688fbf82286f8a8d985c76193c570ea7b0bd2
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-11-05 11:36:28 +0700

    feat: pdf viewer
    chore: rename index.js

---
+++ src/components/PDFViewer/index.jsx
@@ -0,0 +1,135 @@
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import ErrorBoundary from '@docusaurus/ErrorBoundary';
+import Head from '@docusaurus/Head';
+import { Icon } from '@iconify/react';
+import { Button, Stack, Title } from '@mantine/core';
+import React, { useEffect, useState } from 'react';
+
+function previewFile(url, title, embedMode, divId = 'adobe-dc-view') {
+  // eslint-disable-next-line no-undef
+  const adobeDCView = new AdobeDC.View({
+    clientId: 'e11a56067bc646fcbbf5b6486f14283c',
+    divId,
+  });
+  adobeDCView.previewFile(
+    {
+      content: {
+        location: {
+          url,
+        },
+      },
+      metaData: { fileName: title },
+    },
+    {
+      embedMode,
+    },
+  );
+  adobeDCView.registerCallback(
+    /* Type of call back */
+    // eslint-disable-next-line no-undef
+    AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
+    /* call back function */
+    (event) => {
+      if (event.type === 'APP_RENDERING_FAILED') {
+        throw new Error('APP_RENDERING_FAILED');
+      }
+    },
+    /* options to control the callback execution */
+  );
+}
+
+function PDFViewerWrapper({ children }) {
+  return (
+    <BrowserOnly>
+      {() => {
+        return (
+          <ErrorBoundary
+            fallback={({ tryAgain }) => (
+              <Stack align="center">
+                <Title order={1}>This component crashed</Title>
+                <Button onClick={tryAgain}>Try Again!</Button>
+              </Stack>
+            )}
+          >
+            {children}
+          </ErrorBoundary>
+        );
+      }}
+    </BrowserOnly>
+  );
+}
+
+function PDFViewer({ url, title, embedMode = 'FULL_WINDOW' }) {
+  useEffect(() => {
+    const handleLoad = () => {
+      previewFile(url, title, embedMode);
+    };
+
+    document.addEventListener('adobe_dc_view_sdk.ready', handleLoad);
+
+    return () => {
+      document.removeEventListener('adobe_dc_view_sdk.ready', handleLoad);
+    };
+  }, []);
+
+  return (
+    <PDFViewerWrapper>
+      <Head>
+        <script src="https://documentservices.adobe.com/view-sdk/viewer.js"></script>
+      </Head>
+
+      <div id="adobe-dc-view" style={{ height: '100%', width: '100%' }} />
+    </PDFViewerWrapper>
+  );
+}
+
+function PDFViewerSimple({ url, title }) {
+  return (
+    <iframe
+      title={title}
+      src={url}
+      style={{ height: '100%', width: '100%' }}
+    ></iframe>
+  );
+}
+
+function PDFViewerButton({ url, title }) {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsReady(true);
+    };
+
+    document.addEventListener('adobe_dc_view_sdk.ready', handleLoad);
+
+    return () => {
+      document.removeEventListener('adobe_dc_view_sdk.ready', handleLoad);
+    };
+  }, []);
+
+  return (
+    <PDFViewerWrapper>
+      <Head>
+        <script src="https://documentservices.adobe.com/view-sdk/viewer.js"></script>
+      </Head>
+
+      {/* NOTE: width and height have o set to 0 to prevent content reflow */}
+      <div id="adobe-dc-view-btn" style={{ width: 0, height: 0 }} />
+      <Button
+        variant="light"
+        leftIcon={
+          <Icon icon="ant-design:file-pdf-twotone" width={24} height={24} />
+        }
+        disabled={!isReady}
+        onClick={() => {
+          previewFile(url, title, 'LIGHT_BOX', 'adobe-dc-view-btn');
+        }}
+      >
+        View PDF
+      </Button>
+    </PDFViewerWrapper>
+  );
+}
+
+export { PDFViewer, PDFViewerSimple, PDFViewerButton };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,9 @@
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { Button, useMantineColorScheme } from '@mantine/core';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import { PDFViewerButton } from '@site/src/components/PDFViewer/index';
+import sample from '@site/static/pdf/sample.pdf';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
 import React from 'react';
@@ -16,7 +19,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--secondary button--lg !bg-red-900"
             to="/docs/intro"
           >
             Docusaurus Tutorial - 5min ⏱️
@@ -28,6 +31,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
+  // const { toggleColorScheme } = useMantineColorScheme();
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
@@ -36,7 +40,14 @@ export default function Home() {
     >
       <HomepageHeader />
       <main>
+        {/* <div className="w-500 h-500">
+        </div> */}
+        <PDFViewerButton url={sample} title="foo" embedMode="FULL_WINDOW" />
         <HomepageFeatures />
+        <div className="bg-red-400 dark:bg-blue-900 h-40 w-40 animate-animated animate-infinite animate-bounce">
+          Hello
+        </div>
+        {/* <Button onClick={() => toggleColorScheme()}>Foo</Button> */}
       </main>
     </Layout>
   );
