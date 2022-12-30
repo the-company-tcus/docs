a3a714a72a644d1e33ccb8d2d351dffe0238ce90
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2022-12-30 13:25:35 +0700

    feat: add date picker for release viewer page

--- src/pages/releases/_view.jsx
+++ src/pages/releases/_view.jsx
@@ -1,15 +1,17 @@
 import { Icon } from '@iconify/react';
-import { Anchor, Container, Text, Title } from '@mantine/core';
+import { Anchor, Container, Group, Input, Text, Title } from '@mantine/core';
 import { ReleaseList } from '@site/src/components/modules/Release/ReleaseList';
 import moment from 'moment';
 import React, { useMemo } from 'react';
-import { useLocation, useRouteMatch } from 'react-router-dom';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
 
 const ViewRelease = () => {
+  const history = useHistory();
+
   const match = useRouteMatch();
   const { owner, repo } = match.params;
 
-  const { search } = useLocation();
+  const { pathname, search } = useLocation();
   const query = useMemo(() => new URLSearchParams(search), [search]);
 
   const fromTime = moment(query.get('from'), 'MM-DD-YYYY');
@@ -24,10 +26,50 @@ const ViewRelease = () => {
           {owner}/{repo}
         </Anchor>
       </Title>
-      <Text fs="italic" fw={700}>
-        Time: {fromTime.isValid() && `from ${fromTime.format('ll')} `}
-        {toTime.isValid() ? `to ${toTime.format('ll')}` : 'to Current'}
-      </Text>
+      <Group>
+        <Text fs="italic" fw={700}>
+          Time:{' '}
+        </Text>
+        <Group spacing="xs">
+          <Input
+            type="date"
+            id="from"
+            name="from"
+            max={toTime.isValid() ? toTime.format('YYYY-MM-DD') : ''}
+            value={fromTime.isValid() ? fromTime.format('YYYY-MM-DD') : ''}
+            onChange={(e) => {
+              // e.target.value is a string in the format YYYY-MM-DD, empty
+              // string if no date is selected
+              const fromDate =
+                e.target.value !== ''
+                  ? moment(e.target.value, 'YYYY-MM-DD').format('MM-DD-YYYY')
+                  : '';
+
+              query.set('from', fromDate);
+
+              history.push({ pathname, search: query.toString() });
+            }}
+          />
+          {'-'}
+          <Input
+            type="date"
+            id="to"
+            name="to"
+            min={fromTime.isValid() ? fromTime.format('YYYY-MM-DD') : ''}
+            value={toTime.isValid() ? toTime.format('YYYY-MM-DD') : ''}
+            onChange={(e) => {
+              const toDate =
+                e.target.value !== ''
+                  ? moment(e.target.value, 'YYYY-MM-DD').format('MM-DD-YYYY')
+                  : '';
+
+              query.set('to', toDate);
+
+              history.push({ pathname, search: query.toString() });
+            }}
+          />
+        </Group>
+      </Group>
       <ReleaseList owner={owner} repo={repo} from={fromTime} to={toTime} />
     </Container>
   );
