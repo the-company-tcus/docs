20309a5bca39e483b077eec88253c24f105c87d6
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-01-03 23:09:28 +0700

    fix(remark-transform-video): cleanup codes with test function

--- src/remark/transformVideo.js
+++ src/remark/transformVideo.js
@@ -36,6 +36,14 @@ function checkPatterns(patterns, url) {
   });
 }
 
+function isURL(url) {
+  const urlRegex = new RegExp(
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/g,
+  );
+
+  return urlRegex.test(url);
+}
+
 function plugin(options) {
   let patterns = [...DEFAULT_PATTERN];
 
@@ -54,41 +62,41 @@ function plugin(options) {
   async function transformer(tree) {
     const { visit } = await import('unist-util-visit');
 
-    visit(tree, 'link', (node, index, parent) => {
-      const urlRegex = new RegExp(
-        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/g,
-      );
+    const test = (node) => {
+      const checkType = ['link', 'text'];
 
-      if (node.url && node.url.match(urlRegex)) {
-        // Check for support video URL
-        if (patterns && !checkPatterns(patterns, node.url)) {
-          return;
-        }
-
-        const url = node.url.match(urlRegex)[0];
-
-        // NOTE: Set data-type-iframe to 'video' to be able to differentiate between
-        // video and normal iframe
-        const newValue = `<iframe data-type-iframe="video" width="100%" height="100%" style="aspect-ratio: 16/9;" src="${url}" title="Video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`; // iframe copied from youtube share button
-
-        // We swap parent (usually "paragraph" node) with the new value if there
-        // is only one child as there is no others "text" node in the
-        // "paragraph" node
-        if (parent.children.length === 1) {
-          parent.type = 'html';
-          parent.value = newValue;
-          parent.children = [];
-          return;
-        }
-
-        // We only swap the 'link' node with the new value if node is not the
-        // only child
-        node.type = 'html';
-        node.value = newValue;
-        node.children = [];
-        delete node.url;
-        delete node.title;
+      if (!checkType.includes(node.type)) {
+        return false;
       }
+
+      const checkValue = node.value ? node.value : node.url;
+
+      return isURL(checkValue) && checkPatterns(patterns, checkValue);
+    };
+
+    visit(tree, test, (node, index, parent) => {
+      const url = node.value ? node.value : node.url;
+      // NOTE: Set data-type-iframe to 'video' to be able to differentiate between
+      // video and normal iframe
+      const newValue = `<iframe data-type-iframe="video" width="100%" height="100%" style="aspect-ratio: 16/9;" src="${url}" title="Video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`; // iframe copied from youtube share button
+
+      // We swap parent (usually "paragraph" node) with the new value if there
+      // is only one child as there is no others "text" node in the
+      // "paragraph" node
+      if (parent.children.length === 1) {
+        parent.type = 'html';
+        parent.value = newValue;
+        parent.children = [];
+        return;
+      }
+
+      // We only swap the 'link' node with the new value if node is not the
+      // only child
+      node.type = 'html';
+      node.value = newValue;
+      node.children = [];
+      delete node.url;
+      delete node.title;
     });
   }
   return transformer;
