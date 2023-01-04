d684a22ce02a760bfc4db47be05b7453ea1e54c6
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-01-04 20:07:22 +0700

    fix(build): temporarily disable remark-transform-video plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin');
-const transformVideo = require('./src/remark/transformVideo');
+// const transformVideo = require('./src/remark/transformVideo');
 
 require('dotenv').config();
 
@@ -82,7 +82,7 @@ const config = {
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
           beforeDefaultRemarkPlugins: [
-            [transformVideo, { patterns: ['youtube.com'] }],
+            // [transformVideo, { patterns: ['youtube.com'] }],
           ],
         },
         blog: {
