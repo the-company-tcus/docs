da72a54be4d64a9fc0f70841a73d02953c1c2ed5
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-01-04 10:48:42 +0700

    fix: auto add controls for VideoPlayer component

--- src/components/elements/VideoPlayer.jsx
+++ src/components/elements/VideoPlayer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 
 const VideoPlayer = ({ src, ...props }) => {
-  return <ReactPlayer url={src} {...props} />;
+  return <ReactPlayer url={src} controls {...props} />;
 };
 
 export { VideoPlayer };
