9d577fc708d608880856d57862b8d79e3c279158
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-01-09 21:10:26 +0700

    refactor: migrate remark-transform-video to remark-url-to-iframe

--- src/remark/transformVideo.js
+++ src/remark/transformVideo.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-useless-escape */
-/* eslint-disable prefer-regex-literals */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const transformURL = require('./transformURL');
 
 // Ref: https://cookpete.com/react-player/
 const DEFAULT_PATTERN = [
@@ -23,83 +23,27 @@ const DEFAULT_PATTERN = [
   'kaltura.com',
 ];
 
-function checkPatterns(patterns, url) {
-  return patterns.some((pattern) => {
-    if (typeof pattern === 'string') {
-      return url.includes(pattern);
-    }
-    // Check if patterns is a regex
-    if (Object.prototype.toString.call(pattern) === '[object RegExp]') {
-      return pattern.test(url);
-    }
-    return false;
-  });
-}
-
-function isURL(url) {
-  const urlRegex = new RegExp(
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/g,
-  );
-
-  return urlRegex.test(url);
-}
-
 function plugin(options) {
-  let patterns = [...DEFAULT_PATTERN];
-
-  if (options) {
-    const { patterns: customPatterns = [] } = options;
-    if (Array.isArray(customPatterns) === false) {
-      throw new Error(
-        'remark-plugin-transform-video: Patterns should be an array',
-      );
-    }
-
-    patterns = [...DEFAULT_PATTERN, ...customPatterns];
-  }
-
-  // NOTE: Create function inside the plugin to access options
-  async function transformer(tree) {
-    const { visit } = await import('unist-util-visit');
-
-    const test = (node) => {
-      const checkType = ['link', 'text'];
-
-      if (!checkType.includes(node.type)) {
-        return false;
-      }
-
-      const checkValue = node.value ? node.value : node.url;
-
-      return isURL(checkValue) && checkPatterns(patterns, checkValue);
-    };
-
-    visit(tree, test, (node, index, parent) => {
-      const url = node.value ? node.value : node.url;
-      // NOTE: Set data-type-iframe to 'video' to be able to differentiate between
-      // video and normal iframe
-      const newValue = `<iframe data-type-iframe="video" width="100%" height="100%" style="aspect-ratio: 16/9;" src="${url}" title="Video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`; // iframe copied from youtube share button
-
-      // We swap parent (usually "paragraph" node) with the new value if there
-      // is only one child as there is no others "text" node in the
-      // "paragraph" node
-      if (parent.children.length === 1) {
-        parent.type = 'html';
-        parent.value = newValue;
-        parent.children = [];
-        return;
-      }
-
-      // We only swap the 'link' node with the new value if node is not the
-      // only child
-      node.type = 'html';
-      node.value = newValue;
-      node.children = [];
-      delete node.url;
-      delete node.title;
+  return async (tree) => {
+    const transformer = transformURL({
+      patterns: [...DEFAULT_PATTERN, ...(options?.patterns || [])],
+      iframeAttrs: {
+        // NOTE: Set data-type-iframe to 'video' to be able to differentiate
+        // between video and normal iframe
+        'data-type-iframe': 'video',
+        width: '100%',
+        height: '100%',
+        style: 'aspect-ratio: 16/9;',
+        title: 'Video player',
+        frameborder: 0,
+        allow:
+          'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
+        allowfullscreen: true,
+      },
     });
-  }
-  return transformer;
+
+    await transformer(tree);
+  };
 }
 
 module.exports = plugin;
