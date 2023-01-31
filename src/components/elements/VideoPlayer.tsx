1573bbbf1d4384c7de3b1cf644c8f9036620d46c
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-01-31 23:11:51 +0700

    feat: migrate VideoPlayer component to ts

--- src/components/elements/VideoPlayer.tsx
+++ src/components/elements/VideoPlayer.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
+import type { ReactPlayerProps } from 'react-player';
 
-const VideoPlayer = ({ src, ...props }) => {
+const VideoPlayer = ({
+  src,
+  ...props
+}: {
+  src: string;
+} & ReactPlayerProps) => {
   return <ReactPlayer controls url={src} {...props} />;
 };
 
